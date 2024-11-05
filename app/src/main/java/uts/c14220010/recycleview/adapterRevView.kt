497a93a2ee9c2f03cc315a2718aad33d3ca7d3e8
PickleNoToxic <matthew.kevin03@gmail.com>
2024-11-05 20:36:42 +0700

    Initial commit

---
+++ app/src/main/java/uts/c14220010/recycleview/adapterRevView.kt
@@ -0,0 +1,35 @@
+package uts.c14220010.recycleview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.lang.reflect.Array
+
+class adapterRevView (private val listWayang: ArrayList<wayang>) : RecyclerView
+.Adapter<adapterRevView.ListViewHolder>(){
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
+        var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
+        var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
+        var _gambarWayang = itemView.findViewById<TextView>(R.id.gambarWayang)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recycler, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listWayang.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        var wayang = listWayang[position]
+        holder._namaWayang.text = wayang.nama
+        holder._karakterWayang.text = wayang.karakter
+        holder._deskripsiWayang.text = wayang.deskripsi
+    }
+}
---
+++ app/src/main/java/uts/c14220010/recycleview/wayang.kt
@@ -0,0 +1,8 @@
+package uts.c14220010.recycleview
+
+data class wayang(
+    var foto: String,
+    var nama: String,
+    var karakter: String,
+    var deskripsi: String
+)
