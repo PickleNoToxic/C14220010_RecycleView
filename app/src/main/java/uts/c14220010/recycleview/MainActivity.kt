aca55e4de90e6eee44805c373de12c9055ce5bd0
PickleNoToxic <matthew.kevin03@gmail.com>
2024-11-19 18:53:56 +0700

    10.03. RecyclerView - Intent

--- app/src/main/java/uts/c14220010/recycleview/MainActivity.kt
+++ app/src/main/java/uts/c14220010/recycleview/MainActivity.kt
@@ -1,6 +1,9 @@
 package uts.c14220010.recycleview
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -56,6 +59,16 @@ class MainActivity : AppCompatActivity() {
 
     fun tampilkanData(){
         _rvWayang.layoutManager = GridLayoutManager(this, 2)
-        _rvWayang.adapter = adapterRevView(arWayang)
+        val adapterWayang = adapterRevView(arWayang)
+        _rvWayang.adapter = adapterWayang
+
+        adapterWayang.setOnItemClickCallback(object: adapterRevView.onItemClickCallback{
+            override fun onItemClicked(data: wayang) {
+//                Toast.makeText(this@MainActivity, data.nama, Toast.LENGTH_LONG).show()
+                val intent = Intent(this@MainActivity, detWayang::class.java)
+                intent.putExtra("kirimData", data)
+                startActivity(intent)
+            }
+        })
     }
 }
--- app/src/main/java/uts/c14220010/recycleview/wayang.kt
+++ app/src/main/java/uts/c14220010/recycleview/wayang.kt
@@ -1,8 +1,13 @@
 package uts.c14220010.recycleview
 
+import android.os.Parcelable
+
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class wayang(
     var foto: String,
     var nama: String,
     var karakter: String,
     var deskripsi: String
-)
+) : Parcelable
