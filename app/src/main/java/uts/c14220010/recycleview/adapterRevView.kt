4aad472b4ea6d52063055a962334511af0d1d22b
PickleNoToxic <matthew.kevin03@gmail.com>
2024-11-19 18:10:24 +0700

    Finishing

--- app/src/main/java/uts/c14220010/recycleview/adapterRevView.kt
+++ app/src/main/java/uts/c14220010/recycleview/adapterRevView.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import java.lang.reflect.Array
@@ -36,5 +37,9 @@ class adapterRevView (private val listWayang: ArrayList<wayang>) : RecyclerView
         holder._deskripsiWayang.setText(wayang.deskripsi)
         Log.d("gambar", wayang.foto)
         Picasso.get().load(wayang.foto).into(holder._gambarWayang)
+
+        holder._gambarWayang.setOnClickListener {
+            Toast.makeText(holder.itemView.context, wayang.nama, Toast.LENGTH_LONG).show()
+        }
     }
 }
